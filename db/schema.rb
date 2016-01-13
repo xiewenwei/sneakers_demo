7c004f5a3c02ed4a2b368941b1a77d065a162f4c
vincent <vincent@boohee.com>
2016-01-13 23:49:11 +0800

    Replace sqlite with mysql.

--- db/schema.rb
+++ db/schema.rb
@@ -14,11 +14,11 @@
 ActiveRecord::Schema.define(version: 20151021163438) do
 
   create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
+    t.string   "title",      limit: 255
+    t.text     "body",       limit: 65535
     t.boolean  "published"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
 end
